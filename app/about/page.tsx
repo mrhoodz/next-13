d510054b3e3ab16162fb612f33665b0c662fe4ca
mrhoodz <hoodzt7@gmail.com>
2023-01-01 19:39:35 +0200

    pushed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,43 +1,26 @@
-import React from 'react'
-import Components from './(components)'
-import axios from 'axios';
-
-
-
+import React from "react";
+import Components from "./(components)";
 
 async function getProduct() {
-
   interface productProps {
     title: string;
     description: string;
     price: number;
-    
-    
-    }
-
+  }
 
   try {
-    const response = await axios.get("https://dummyjson.com/products/1");
-    return response.data as productProps;
-    console.log(response.data);
+    const response = await fetch("https://dummyjson.com/products/1");
+    const data = await response.json();
+    return data as productProps;
+    // console.log(data);
   } catch (error) {
     console.error(error);
   }
 }
 
-
-
-
 export default async function page() {
-
-
-
-
-
   const products = await getProduct();
   console.log(products);
-  
-  return (
-    <Components />
-  )
+
+  return <Components />;
 }
